33a365da914ff90d91cbba57aa1be3dec9952c11
baolinhdev <baolinhdev@gmail.com>
2024-10-07 21:20:04 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/ball_magic_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ball_magic_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '8 Ball Magic',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const BallPage(),
+    );
+  }
+}
+
+class BallPage extends StatefulWidget {
+  const BallPage({super.key});
+
+  @override
+  State<BallPage> createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ballNumber = 1;
+
+  void changeBall() {
+    setState(() {
+      // Randomly choose a number between 1 and 5
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('8 Ball Magic'),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Ask me anything!',
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            const SizedBox(height: 20),
+            TextButton(
+              onPressed: changeBall,
+              child: Image.asset('images/ball$ballNumber.png'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
